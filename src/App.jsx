1a3ca270142cd73f7e10e01b78ab9019970e93d0
saravana0204 <saravanakumarvijaymask@gmail.com>
2024-09-30 21:47:57 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='main'>
+      <h1>SARAVANAKUMAR😍</h1>
+      <h2>Software Developer👨‍💻</h2>
+      <h3> Email:  saravanakumarvijaymask@gmail.com<br></br>
+     Address:Trichy,Tamil Nadu<br></br>
+      Phone No:   9345745394<br></br>
+     <a href='https://github.com/saravana0204'> Github:https://github.com/saravana0204</a><br></br>
+       </h3>
+     <h1>Summary</h1>
+       <p>  Seeking an entry level opportinity with an esteemed organization where i can utilize my skills and enhance learning in the field of work capable of mastering new technologies </p>
+       <h4>Technical Skills🧠</h4>
+       <ul>
+        <li>Html</li>
+        <li>Css</li>
+        <li>Java Script</li>
+       </ul>
+<h5>Education☠️</h5> 
+<li>VETRI VINAYAHA COLLEGE OF ENGINEERING AND TECHNOLOGY</li>
+<li>Nov 2021-Nov 2025 </li> 
+<li>80%</li>   
+<h1 className='skills'>Skills🤡</h1>
+<li>LeaderShip</li>
+<li>Quick learner</li>
+<li>Self Respect</li>
+<hr></hr>
+
+
+   </div>
+  )
+}
+
+export default App
